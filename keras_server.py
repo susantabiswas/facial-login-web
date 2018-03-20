beeb96549bb40d6810eb8d68253dd1b883a72237
susantabiswas <susanta18@outlook.com>
2018-03-20 18:29:56 +0530

    port changed

--- keras_server.py
+++ keras_server.py
@@ -185,4 +185,4 @@ if __name__ == "__main__":
 	print('Model loaded..............')
 	ini_user_database()
 	print('Database loaded...........')
-	app.run(host='localhost', port=5000)
+	app.run(host='0.0.0.0', port=5000)
