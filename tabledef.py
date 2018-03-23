c90f85ca3b74e7dd13065b62ed4b72285515bb72
susantabiswas <susanta18@outlook.com>
2018-03-23 22:56:19 +0530

    table structure script

---
+++ tabledef.py
@@ -0,0 +1,26 @@
+from sqlalchemy import *
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy import Column, Date, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+ 
+engine = create_engine('sqlite:///tutorial.db', echo=True)
+Base = declarative_base()
+ 
+########################################################################
+class User(Base):
+    """"""
+    __tablename__ = "users"
+ 
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    password = Column(String)
+ 
+    #----------------------------------------------------------------------
+    def __init__(self, username, password):
+        """"""
+        self.username = username
+        self.password = password
+ 
+# create tables
+Base.metadata.create_all(engine)
